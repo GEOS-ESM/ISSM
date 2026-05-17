7fe5649eccee9aef141564865840bd855ac0375c
agstub <aaron.g.stubblefield@dartmouth.edu>
2026-05-17 16:40:41 -0400

    include prefixes in example sh

--- examples/GEOSInput/generate_antarctica_input.sh
+++ examples/GEOSInput/generate_antarctica_input.sh
@@ -1,5 +1,5 @@
 source issm_env_py
-rm -f AntarcticaGEOS.bin AntarcticaGEOS.outbin AntarcticaGEOS.outlog AntarcticaGEOS.errlog
+rm -f ISSM_AntarcticaGEOS.bin ISSM_AntarcticaGEOS.outbin ISSM_AntarcticaGEOS.outlog ISSM_AntarcticaGEOS.errlog
 (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python Antarctica_input_control.py)
-${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) AntarcticaGEOS 2>> AntarcticaGEOS.errlog
+${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) ISSM_AntarcticaGEOS 2>> ISSM_AntarcticaGEOS.errlog
 (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python Antarctica_input_transient.py)
--- examples/GEOSInput/generate_greenland_input.sh
+++ examples/GEOSInput/generate_greenland_input.sh
@@ -1,5 +1,5 @@
 source issm_env_py
-rm -f GreenlandGEOS.bin GreenlandGEOS.outbin GreenlandGEOS.outlog GreenlandGEOS.errlog
+rm -f ISSM_GreenlandGEOS.bin ISSM_GreenlandGEOS.outbin ISSM_GreenlandGEOS.outlog ISSM_GreenlandGEOS.errlog
 (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python Greenland_input_control.py)
-${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) GreenlandGEOS 2>> GreenlandGEOS.errlog
+${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) ISSM_GreenlandGEOS 2>> ISSM_GreenlandGEOS.errlog
 (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python Greenland_input_transient.py)
